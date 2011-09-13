c52cb6f918baa562e363a22d939b76c0f960fafe
Graeme Mathieson <mathie@woss.name>
2011-09-13 19:52:24 +0100

    rails new fuel -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fuel::Application.config.secret_token = '87ff4024a248c480d875668fc966eae72a0388f4498a18892982dfac95451c03d721f3686793ac32136f511a65b737863db2bf4a9a8bcc86b08b6eaec8aaddd8'
